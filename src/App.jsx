e55258d04436a33f16885e6c20a9504e1588b6a6
Ievgen Petryk <jakeshad123@gmail.com>
2025-10-02 22:34:56 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,34 @@ export const goods = [
 ];
 
 export const App = () => {
-  const [goodName, setGoodName] = useState('Jam');
+  const [selectedGood, setSelectedGood] = useState('Jam');
+
+  const handleClear = () => {
+    setSelectedGood('');
+  };
+
+  const handleSelect = (good) => {
+    setSelectedGood(good);
+  };
+
+  const handleRemove = () => {
+    setSelectedGood('');
+  };
 
   return (
     <main className="section container">
-      {goodName === '' ? (
+      {selectedGood === '' ? (
         <h1 className="title is-flex is-align-items-center">
           No goods selected
         </h1>
       ) : (
         <h1 className="title is-flex is-align-items-center">
-          {goodName} is selected
+          {selectedGood} is selected
           <button
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => setGoodName('')}
+            onClick={handleClear}
           />
         </h1>
       )}
@@ -40,33 +52,29 @@ export const App = () => {
         <tbody>
           {goods.map(good => {
             const goodClassName =
-              good === goodName ? 'has-background-success-light' : '';
+              good === selectedGood ? 'has-background-success-light' : '';
 
             return (
-              <tr data-cy="Good" className={goodClassName}>
+              <tr data-cy="Good" className={goodClassName} key={good}>
                 <td>
-                  {goodName !== good ? (
+                  {selectedGood !== good ? (
                     <button
                       data-cy="AddButton"
                       type="button"
                       className="button"
-                      onClick={() => {
-                        setGoodName(good);
-                      }}
+                      onClick={() => handleSelect(good)}
                     >
                       +
                     </button>
                   ) : (
-                    <td>
-                      <button
-                        data-cy="RemoveButton"
-                        type="button"
-                        className="button is-info"
-                        onClick={() => setGoodName('')}
-                      >
-                        -
-                      </button>
-                    </td>
+                    <button
+                      data-cy="RemoveButton"
+                      type="button"
+                      className="button is-info"
+                      onClick={handleRemove}
+                    >
+                      -
+                    </button>
                   )}
                 </td>
 
